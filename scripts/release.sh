0895f574dd5bd6693aec0237166e221b10a6e858
Kevin Conroy <kconroy@globalgiving.org>
2018-03-14 09:57:04 -0400

    Updated RC 1.31

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,10 @@
 cd ..
-rm -rf release
-mkdir release
-cp -r fonts/ttf release/ttf
-cp -r fonts/otf release/otf
-cp OFL.txt release/OFL.txt
-cp FONTLOG.txt release/FONTLOG.txt
-cp README.md release/README.md
+rm -rf build/aleo-131
+mkdir build
+mkdir build/aleo-131
+cp -r fonts/ttf build/aleo-131/ttf
+cp -r fonts/otf build/aleo-131/otf
+cp OFL.txt build/aleo-131/OFL.txt
+cp FONTLOG.txt build/aleo-131/FONTLOG.txt
+cp README.md build/aleo-131/README.md
 
