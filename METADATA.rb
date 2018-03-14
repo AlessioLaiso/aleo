ec019acd25c7a801f15d245915543f97b39236eb
Kevin Conroy <kconroy@globalgiving.org>
2018-03-13 22:47:27 -0400

    Added metadata

---
+++ METADATA.rb
@@ -0,0 +1,68 @@
+{
+  "name": "Aleo",
+  "designer": "Alessio Laiso",
+  "license": "OFL",
+  "visibility": "Sandbox",
+  "category": "Sans Serif",
+  "fonts": [
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-Regular",
+      "fullName": "Aleo",
+      "style": "normal",
+      "weight": 400,
+      "filename": "Aleo-Regular.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-Bold",
+      "fullName": "Aleo",
+      "style": "normal",
+      "weight": 700,
+      "filename": "Aleo-Bold.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },    
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-Light",
+      "fullName": "Aleo",
+      "style": "normal",
+      "weight": 300,
+      "filename": "Aleo-Light.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },    
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-RegularItalic",
+      "fullName": "Aleo",
+      "style": "italic",
+      "weight": 400,
+      "filename": "Aleo-Regular.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-BoldItalic",
+      "fullName": "Aleo",
+      "style": "italic",
+      "weight": 700,
+      "filename": "Aleo-BoldItalic.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },    
+    {
+      "name": "Aleo",
+      "postScriptName": "Aleo-LightItalic",
+      "fullName": "Aleo",
+      "style": "italic",
+      "weight": 300,
+      "filename": "Aleo-LightItalic.ttf",
+      "copyright": "Copyright (c) 2013-2018 by Alessio Laiso (hello@alessiolaiso.com)"
+    },    
+  ],
+  "subsets": [
+    "latin",
+    "menu"
+  ],
+  "dateAdded": "2018-03-13"
+}
