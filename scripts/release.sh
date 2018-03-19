5acb9c9f3ee1eefa8a4561a7b2d5b66041e36762
Kevin Conroy <kconroy@globalgiving.org>
2018-03-19 10:35:49 -0400

    Minor tweaks to winAscent, winDecent, updated copyright for Google fonts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 cd ..
-rm -rf build/aleo-131
+rm -rf build
 mkdir build
 mkdir build/aleo-131
 cp -r fonts/ttf build/aleo-131/ttf
@@ -8,3 +8,6 @@ cp OFL.txt build/aleo-131/OFL.txt
 cp FONTLOG.txt build/aleo-131/FONTLOG.txt
 cp README.md build/aleo-131/README.md
 
+cd build
+zip -r aleo-131.zip aleo-131
+cp aleo-131.zip ../release
