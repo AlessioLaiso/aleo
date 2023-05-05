8c3b43bbbee54206dc4b4e29e7652d54210705e1
Kevin Conroy <kconroy@globalgiving.org>
2023-05-04 21:13:18 -0400

    Updated scripts for v2.0.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,13 @@
 cd ..
 rm -rf build
 mkdir build
-mkdir build/aleo-131
-cp -r fonts/ttf build/aleo-131/ttf
-cp -r fonts/otf build/aleo-131/otf
-cp OFL.txt build/aleo-131/OFL.txt
-cp FONTLOG.txt build/aleo-131/FONTLOG.txt
-cp README.md build/aleo-131/README.md
+mkdir build/aleo-200
+cp -r fonts/ttf build/aleo-200/ttf
+cp -r fonts/otf build/aleo-200/otf
+cp OFL.txt build/aleo-200/OFL.txt
+cp FONTLOG.txt build/aleo-200/FONTLOG.txt
+cp README.md build/aleo-200/README.md
 
 cd build
-zip -r aleo-131.zip aleo-131
-cp aleo-131.zip ../release
+zip -r aleo-200.zip aleo-200
+cp aleo-200.zip ../release
